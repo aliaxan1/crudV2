ac9442031fb93a614734a124e43b1f81c5bd1b6c
Muhammad Azan Sajid <azan.developer@gmail.com>
2023-07-31 06:29:52 +0000

    complete 1

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
-async function apiData() {
+let abc = null;
+
+async function getData() {
     const response = await fetch("https://reqres.in/api/users");
     const data = await response.json();
-    return data.data;
-  }
-apiData().then((data) => {});
+    let reqData = data.data;
+    abc = reqData;
+    return reqData;
+}
+
+getData()
+    .then((data) => {
+        console.log(data);
+        abc = data;
+
+        // Add data from API response to the table
+        for (let i = 0; i < abc.length; i++) {
+            addRowToTable(abc[i]);
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+
+
+
+
+
+
 // Function to add a new row to the table
+
 function addRowToTable(data) {
     var table = document.getElementsByTagName('tbody')[0];
     var row = table.insertRow();
@@ -17,27 +42,26 @@ function addRowToTable(data) {
     var phoneNoCell = row.insertCell();
     var imageCell = row.insertCell();
 
-    firstNameCell.innerHTML = data.firstName;
-    lastNameCell.innerHTML = data.lastName;
-    rollNoCell.innerHTML = data.rollNo;
+    firstNameCell.innerHTML = data.first_name;
+    lastNameCell.innerHTML = data.last_name;
+    rollNoCell.innerHTML = data.id;
     cityCell.innerHTML = data.city;
-    phoneNoCell.innerHTML = data.phoneNo;
+    phoneNoCell.innerHTML = data.phone;
     
-    console.log(data.image);
+    console.log(data.avatar);
 
-    if (data.image == "") {
+    if (data.avatar == "") {
         imageCell.innerHTML = `<img src="" alt="image not uploaded" width="100px" height="100px">`;
     }else{
-        imageCell.innerHTML = `<img src="${data.image}" alt="image" width="100px" height="100px">`;
+        imageCell.innerHTML = `<img src="${data.avatar}" alt="image" width="100px" height="100px">`;
     }
     
     
 }
 
-
 // Function to handle form submission
- function formSubmission() {
-    
+document.getElementById("myForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
     
     // Get the form input values
     var firstName = document.getElementById("validationDefault01").value;
@@ -49,23 +73,17 @@ function addRowToTable(data) {
     
     // Create an object to hold the data
     var formData = {
-        firstName: firstName,
-        lastName: lastName,
-        rollNo: rollNo,
+        first_name: firstName,
+        last_name: lastName,
+        id: rollNo,
         city: city,
         phoneNo: phoneNo,
-        image: image
+        avatar: image
     };
     
-    // Add the data to the table
+   
     addRowToTable(formData);
     
-    // Reset the form after submission
+   
     document.getElementById("myForm").reset();
-};
-
-// Function to handle form submission
-document.getElementById("myForm").addEventListener("submit",(event)=> {
-    formSubmission();
-    event.preventDefault(); // Prevent form submission
-});
+});s
